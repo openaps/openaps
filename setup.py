87c975dfc07183cc05a7fb0009ecc530be2e8ac7
Ben West <bewest@gmail.com>
2015-04-11 20:40:26 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+
+import openaps
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+setup(name='openaps',
+    version=openaps.__version__, # http://semver.org/
+    description='Open Source Artificial Pancreas System.',
+    long_description=readme(),
+    author="OpenAPS",
+    author_email="bewest+openaps@gmail.com",
+    # url="https://github.com/openaps/openaps",
+    url="https://openaps.org/",
+    packages=['openaps'],
+    install_requires = [
+      'pyserial', 'python-dateutil', 'argcomplete',
+      'decocare', # 'dexcom_reader'
+    ],
+    dependency_links = [
+      'http://github.com/compbrain/dexcom_reader/tarball/master',
+      'http://github.com/bewest/decoding-carelink/tarball/master',
+    ],
+    scripts = [
+      'bin/openaps',
+      'bin/openaps-enact',
+      'bin/openaps-suggest',
+      'bin/openaps-get',
+    ],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Libraries'
+    ],
+    zip_safe=False,
+)
+
+#####
+# EOF

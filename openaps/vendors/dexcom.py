fbe30c5c34c4f81d31087a4236563edf900b55ef
Ben West <bewest@gmail.com>
2015-04-27 16:09:39 -0700

    add naive dexcom commands

--- openaps/vendors/dexcom.py
+++ openaps/vendors/dexcom.py
@@ -29,6 +29,30 @@ class scan (Use):
   """ scan for usb stick """
   def scanner (self):
     return readdata.Dexcom.FindDevice( )
+  def before_main (self, args, app):
+    self.port = self.scanner( )
+    self.dexcom = self.port and readdata.Dexcom(self.port) or None
   def main (self, args, app):
-    return self.scanner( ) or ''
+    return self.port or ''
 
+@use( )
+class glucose (scan):
+  """  glucose """
+  def prerender_text (self, data):
+    out = [ ]
+    for item in data:
+      line = map(str, [
+        item['display_time']
+      , item['glucose']
+      , item['trend_arrow']
+      ])
+      out.append(' '.join(line))
+    return "\n".join(out)
+  def prerender_JSON (self, data):
+    return data
+  def main (self, args, app):
+    records = self.dexcom.ReadRecords('EGV_DATA')
+    out = [ ]
+    for item in records:
+      out.append(item.to_dict( ))
+    return out

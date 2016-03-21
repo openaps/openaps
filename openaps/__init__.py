9765f1c8757b193c2660f6a815e0e4ea19085dd6
Ben West <bewest@gmail.com>
2016-03-21 02:41:27 -0700

    bump version

--- openaps/__init__.py
+++ openaps/__init__.py
@@ -1,3 +1,11 @@
 
-__version__ = '0.0.11-dev'
+"""
+Release notes:
+
+* 0.1.0 - transition to Exported interfaces, utilize pkg_resources
+          more for advertisements of openaps capabilities across the
+          python environment.  Also, rely more on json import/export
+          of configuration.  Also, variety of git tweaks.
+"""
+__version__ = '0.1.0-dev'
 

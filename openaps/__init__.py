f3218cb6215f36019523bae959b4316f09cdd77a
Scott <scottleibrand@gmail.com>
2017-07-15 13:02:08 -0700

    bump patch version

--- openaps/__init__.py
+++ openaps/__init__.py
@@ -7,5 +7,5 @@ Release notes:
           python environment.  Also, rely more on json import/export
           of configuration.  Also, variety of git tweaks.
 """
-__version__ = '0.2.0-dev'
+__version__ = '0.2.1-dev'
 
